37aeefe52591d270cd94e06bc7a572a33f0a3760
Wes Harrell <wes.harrell@reannz.co.nz>
2026-06-18 18:38:25 +1200

    fix silly backtik issue, note to self, dont try to do markdown in bash :/

--- user/init.sh
+++ user/init.sh
@@ -2,11 +2,11 @@
 if [ ! -f "${HOME}/.spack/config.yaml" ]; then
     mkdir -p "${HOME}/.spack"
     cp /opt/nesi/spack/etc/spack-conf/user/config.yaml "${HOME}/.spack/config.yaml"
-    cp /opt/nesi/spack/etc/spack-conf/user/upstream.yaml "${HOME}/.spack/upstream.yaml"
+    cp /opt/nesi/spack/etc/spack-conf/user/upstreams.yaml "${HOME}/.spack/upstreams.yaml"
     echo "[Spack] Created ~/.spack directory in your home"
     echo "[Spack] Copied config.yaml and upstream.yaml from templates to your ~/.spack dir"
     echo "[Spack] Review and update install_tree in config.yaml to your preferred location"
     echo ""
-    echo "[Spack] Run `spack list` to generate the package list, this will take several minutes"
+    echo '[Spack] Run `spack list` to generate the package list, this will take several minutes'
     echo ""
 fi
