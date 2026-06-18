7c9a095421c2e698de4f87c9a95e777689be940e
Wes Harrell <wes.harrell@reannz.co.nz>
2026-06-18 14:39:28 +1200

    add user init script to be called from module on first load

---
+++ user/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ ! -f "${HOME}/.spack/config.yaml" ]; then
+    mkdir -p "${HOME}/.spack"
+    cp /opt/nesi/spack/etc/spack-conf/user/config.yaml "${HOME}/.spack/config.yaml"
+    cp /opt/nesi/spack/etc/spack-conf/user/upstream.yaml "${HOME}/.spack/upstream.yaml"
+    echo "[Spack] Created ~/.spack directory in your home"
+    echo "[Spack] Copied config.yaml and upstream.yaml from templates to your ~/.spack dir"
+    echo "[Spack] Review and update install_tree in config.yaml to your preferred location"
+    echo ""
+    echo "[Spack] Run `spack list` to generate the package list, this will take several minutes"
+    echo ""
+fi
