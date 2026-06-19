e005290c976b11f95e7c9aaabe9e096826093ddf
Wes Harrell <wes.harrell@reannz.co.nz>
2026-06-19 13:36:43 +1200

    provide some more info on using environments

--- user/functions.sh
+++ user/functions.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-#Spack does not have a generic list available environments so...here is a function to do that
+#Spack does not have a way to list all available environments so...here is a function to do that
 spack-envlist() {
-    echo "=== Site environments ==="
+    echo "=== Site environments in /opt/nesi/spack/zen3/spack-env ==="
     ls /opt/nesi/spack/zen3/spack-env/ 2>/dev/null || echo "none"
-    echo "=== Your environments ==="
+    echo ""
+    echo "=== Below is a list of any environments you created ==="
     spack env list
+    echo ""
+    echo "To track a site environment run:"
+    echo "spack env track /opt/nesi/spack/zen3/spack-env/ENVNAME"
+    echo ""
 }
